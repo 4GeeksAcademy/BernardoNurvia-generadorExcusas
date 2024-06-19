23bf53dcbfe6e7cd9c6d2fdf89affd3f57e65575
BernardoNurvia <167910567+BernardoNurvia@users.noreply.github.com>
2024-06-19 16:36:50 +0000

    primera configuracion

--- src/app.js
+++ src/app.js
@@ -9,3 +9,13 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
