bebf54ad726fd2baa2d33df670fa9583bf0c4e3e
Bernardo Patiño <bap_link@hotmail.com>
2024-06-24 09:53:33 +0000

    my first excuse generator version

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+//creo mis variables
 let who = ["The dog", "My grandma", "The mailman", "My bird"];
 let action = ["ate", "peed", "crushed", "broke"];
 let what = ["my homework", "my phone", "the car"];
@@ -19,4 +20,13 @@ let when = [
   "during my lunch",
   "while I was praying"
 ];
-let excuseGenerator = who[Math];
+// con `${variable[math...]}` llamo a las variables para mi generador
+let excuseGenerator = `${who[Math.floor(Math.random() * who.length)]} ${
+  action[Math.floor(Math.random() * action.length)]
+} ${what[Math.floor(Math.random() * what.length)]} ${
+  when[Math.floor(Math.random() * when.length)]
+}.`;
+//variable para sustituir
+let texExcuse = document.getElementById("excuse");
+// variable que sustitulle
+texExcuse.innerText = excuseGenerator;
